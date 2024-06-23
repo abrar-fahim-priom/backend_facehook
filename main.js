b237c68aff25478663121a1de5eac10b89029bc2
abrar-fahim-priom <priomshads@gmail.com>
2024-06-23 23:26:02 +0600

    removed trailed slash

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const router = jsonServer.router("./database/db.json");
 
 // CORS configuration
 const corsOptions = {
-  origin: "https://frontend-facehook-1.onrender.com/", // Your frontend origin
+  origin: "https://frontend-facehook-1.onrender.com", // Your frontend origin
   methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Allow cookies and other credentials
